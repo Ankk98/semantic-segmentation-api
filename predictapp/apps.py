a07c54a64d00661a96fd06ad2ce741b994cdb8bd
ankk98 <ankk98@gmail.com>
2020-05-14 15:08:29 +0530

    cleaned for restarting the project

--- predictapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PredictappConfig(AppConfig):
-    name = 'predictapp'
--- predictapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', FileUploadView.as_view())
-]
